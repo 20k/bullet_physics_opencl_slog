8bb098d7911b1a3a1445f4728afc26da43411db9
James <Icedshot@gmail.com>
2018-02-03 14:21:31 +0000

    preparing for the long haul

--- main.cpp
+++ main.cpp
@@ -13,6 +13,7 @@
 #include <Bullet3OpenCL/RigidBody/b3GpuNarrowPhaseInternalData.h>
 
 #include <SFML/Graphics.hpp>
+#include <ocl/ocl.hpp>
 
 struct GpuDemoInternalData
 {
@@ -20,6 +21,7 @@ struct GpuDemoInternalData
 	cl_context m_clContext;
 	cl_device_id m_clDevice;
 	cl_command_queue m_clQueue;
+
 	bool m_clInitialized;
 	char*	m_clDeviceName;
 
@@ -35,48 +37,128 @@ struct GpuDemoInternalData
 	}
 };
 
+#define MSTRINGIFY(A) #A
+
+static const char* s_rigidBodyKernelString = MSTRINGIFY(
+
+typedef struct
+{
+	float4 m_pos;
+	float4 m_quat;
+	float4 m_linVel;
+	float4 m_angVel;
+	unsigned int m_collidableIdx;
+	float m_invMass;
+	float m_restituitionCoeff;
+	float m_frictionCoeff;
+} Body;
+
+__kernel void
+	copyTransformsToVBOKernel( __global Body* gBodies, __global float4* posOrnColor, const int numNodes)
+{
+	int nodeID = get_global_id(0);
+	if( nodeID < numNodes )
+	{
+		posOrnColor[nodeID] = (float4) (gBodies[nodeID].m_pos.xyz,1.0);
+		posOrnColor[nodeID + numNodes] = gBodies[nodeID].m_quat;
+	}
+}
+);
+
+
+static const char* dname = "Phony device";
+
+/*struct b3Config
+{
+    int m_maxConvexBodies = 50;
+    int m_maxConvexShapes = 50;
+    int m_maxBroadphasePairs = 8 * m_maxConvexBodies;
+    int m_maxContactCapacity = m_maxBroadphasePairs;
+
+};*/
+
+struct session_data
+{
+    cl_kernel m_copyTransformsToVBOKernel;
+
+    b3Config m_config;
+};
+
+int gGpuArraySizeX = 45;
+int gGpuArraySizeY = 55;
+int gGpuArraySizeZ = 45;
+
+extern int b3OpenCLUtils_clewInit();
+
 struct opencl_base
 {
     struct GpuDemoInternalData*	m_clData;
 
-    void init()
+    session_data* data;
+
+    void initCL()
     {
         m_clData = new GpuDemoInternalData();
 
-        cl_device_type deviceType = CL_DEVICE_TYPE_GPU;
+        data = new session_data;
 
-        int ciErrNum = 0;
+        b3OpenCLUtils_clewInit();
 
-        {
-            m_clData->m_clContext = b3OpenCLUtils::createContextFromType(deviceType, &ciErrNum, 0,0,-1, -1,&m_clData->m_platformId);
-        }
+        cl::context ctx;
+        cl::command_queue cqueue(ctx);
+
+        m_clData->m_clContext = ctx.ccontext;
+        m_clData->m_platformId = ctx.platform;
+        m_clData->m_clDevice = ctx.selected_device;
+        m_clData->m_clQueue = cqueue.cqueue;
+
+        m_clData->m_clInitialized = true;
+        m_clData->m_clDeviceName = "PhonyDevice";
+
+        int errNum = 0;
+
+        //cl_program rbProg = 0;
+
+        cl::program prog(ctx, s_rigidBodyKernelString, false);
+        prog.build_with(ctx, "");
+
+        cl::kernel copyTransformsToVBOKernel(prog, "copyTransformsToVBOKernel");
+
+        data->m_copyTransformsToVBOKernel = copyTransformsToVBOKernel.ckernel;
+
+        #endif // 0
 
 
-        oclCHECKERROR(ciErrNum, CL_SUCCESS);
+        data->m_config.m_maxConvexBodies = b3Max(data->m_config.m_maxConvexBodies,gGpuArraySizeX*gGpuArraySizeY*gGpuArraySizeZ+10);
+		data->m_config.m_maxConvexShapes = data->m_config.m_maxConvexBodies;
+		int maxPairsPerBody = 16;
+		data->m_config.m_maxBroadphasePairs = maxPairsPerBody*data->m_config.m_maxConvexBodies;
+		data->m_config.m_maxContactCapacity = data->m_config.m_maxBroadphasePairs;
 
-        int numDev = b3OpenCLUtils::getNumDevices(m_clData->m_clContext);
+        b3GpuNarrowPhase* np = new b3GpuNarrowPhase(m_clData->m_clContext,m_clData->m_clDevice,m_clData->m_clQueue,data->m_config);
+		b3GpuBroadphaseInterface* bp =0;
 
-        if (numDev>0)
-        {
-            m_clData->m_clDevice= b3OpenCLUtils::getDevice(m_clData->m_clContext,0);
-            m_clData->m_clQueue = clCreateCommandQueue(m_clData->m_clContext, m_clData->m_clDevice, 0, &ciErrNum);
-            oclCHECKERROR(ciErrNum, CL_SUCCESS);
+		bool useUniformGrid = false;
 
+		if (useUniformGrid)
+		{
+			bp = new b3GpuGridBroadphase(m_clData->m_clContext,m_clData->m_clDevice,m_clData->m_clQueue);
+		} else
+		{
+			bp = new b3GpuSapBroadphase(m_clData->m_clContext,m_clData->m_clDevice,m_clData->m_clQueue);
+		}
 
-            b3OpenCLDeviceInfo info;
-            b3OpenCLUtils::getDeviceInfo(m_clData->m_clDevice,&info);
-            m_clData->m_clDeviceName = info.m_deviceName;
-            m_clData->m_clInitialized = true;
-
-        }
     }
 };
 
 int main()
 {
+    lg::set_logfile("./out.txt");
+    lg::redirect_to_stdout();
+
     sf::RenderWindow win(sf::VideoMode(800, 600), "openclfun");
 
     opencl_base base;
-    base.init();
+    base.initCL();
 
 }
